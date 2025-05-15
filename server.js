972f2f665e6b337416365bc42d0d8caf8604590e
RashtriyaGaurav <rashtriya.73@gmail.com>
2025-05-15 10:05:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,27 +11,28 @@ app.use(express.static("public"));
 let waitingUser = null;
 
 io.on("connection", (socket) => {
-  if (waitingUser) {
-    // Pair the users
-    socket.to(waitingUser).emit("peer", socket.id);
-    socket.emit("peer", waitingUser);
-    waitingUser = null;
-  } else {
-    waitingUser = socket.id;
-  }
+  socket.on("ready", () => {
+    if (waitingUser) {
+      // Pair them
+      socket.emit("peer", waitingUser);
+      io.to(waitingUser).emit("peer", socket.id);
+      waitingUser = null;
+    } else {
+      waitingUser = socket.id;
+    }
+  });
 
-  socket.on("signal", (data) => {
-    io.to(data.to).emit("signal", { from: socket.id, signal: data.signal });
+  socket.on("signal", ({ to, signal }) => {
+    io.to(to).emit("signal", { from: socket.id, signal });
   });
 
   socket.on("disconnect", () => {
     if (waitingUser === socket.id) {
       waitingUser = null;
     }
-    socket.broadcast.emit("peer-disconnected", socket.id);
   });
 });
 
 server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+  console.log("Server listening on http://localhost:3000");
 });
